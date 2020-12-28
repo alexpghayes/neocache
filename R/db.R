10c7b824bc01c6a622308caad20b168548acc524
Nathan Kolbow <nkolbow@wisc.edu>
2020-12-28 15:04:31 -0600

    Added to start_neo4j and stop_neo4j

--- R/db.R
+++ R/db.R
@@ -10,7 +10,7 @@ get_connexion <- function() {
   con <- neo4j_api$new(
     url = "http://localhost:7474",
     user = "neo4j",
-    password = "password"
+    password = "pass"
   )
 
   if (!(con$ping() == 200)) {
@@ -20,7 +20,28 @@ get_connexion <- function() {
   con
 }
 
-#' TODO
+#' Title
+#' TODO: Add docs
+start_neo4j <- function() {
+  # Start the Docker container
+  # First try to run the existing Docker container
+  if(system("docker start neocache_docker") == 1) {
+    # Container does not exist, create the container
+    if(system("docker run --name neocache_docker -p7474:7474 -p7687:7687 -d --env NEO4J_AUTH=neo4j/pass neo4j:3.5.21") != 0) {
+      warning("Failed to initiate docker container.")
+    }
+  }
+}
+
+#' Title
+#' TODO: Add docs
+stop_neo4j <- function() {
+  if(system("docker stop neocache_docker") == 1) {
+    warning("Error returned when attempting to stop docker container.")
+  }
+}
+
+#' TODO: Add docs
 #'
 #' @param query the CYPHER query to be passed to call_neo4j
 #' @param con the neo4j connection object to be passed to call_neo4j
@@ -38,6 +59,8 @@ sup4j <- function(query, con = get_connexion()) {
 #' HELPER FUNCTION USED TO CLEAR THE DATABASE FOR TESTING,
 #' REMOVE THIS FUNCTION ONCE TESTING IS DONE
 clear____db <- function() {
+  con <- get_connexion()
+
   "MATCH (n) DETACH DELETE n" %>%
     sup4j(con)
 }
