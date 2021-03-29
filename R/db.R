49e7d0b60174e7fcf3fa0454230fa805d66d2fe9
Alex Hayes <alexpghayes@gmail.com>
2021-03-29 11:20:54 -0500

    Some cleanup

--- R/db.R
+++ R/db.R
@@ -11,112 +11,50 @@ get_connexion <- function() {
   con
 }
 
-
 #' Starts the Neo4j Docker container; creates and sets up the container if it
 #' does not already exist.
 #'
 #' @export
 start_neo4j <- function() {
-  message("Checking the status of Docker...")
+  log_debug("Checking the status of Docker...")
   # Check if Docker is running
-  if (system("docker system info", ignore.stdout = TRUE, ignore.stderr = TRUE) != 0) {
+  if (!find_docker()) {
     stop("Docker does not appear to be running. Please start Docker and try again.")
   }
-  message("Docker seems to be running.\n")
+  log_debug("Docker seems to be running.\n")
 
-  message("Attempting to start Neocache container...")
+  log_debug("Attempting to start Neocache container...")
   # Start the Docker container
   # First try to run the existing Docker container
-  if (system("docker start neocache_docker", ignore.stdout = TRUE, ignore.stderr = TRUE) != 0) {
+  if (!start_neocache_docker()) {
     # Container does not exist, create the container
-    if (system("docker run --name neocache_docker -p7474:7474 -p7687:7687 -d -e NEO4J_AUTH=neo4j/pass -e NEO4J_apoc_export_file_enabled=true -e NEO4J_apoc_import_file_enabled=true -e NEO4J_apoc_import_file_use__neo4j__config=true -e NEO4JLABS_PLUGINS=[\\\"apoc\\\"] neo4j:3.5.21",
-      ignore.stdout = TRUE
-    ) != 0) {
+    if (!create_neocache_docker()) {
       stop("Failed to initiate docker container.")
     }
   }
 
-  message(
+  log_debug(
     "Neocache container successfully launched.\n",
     "\nWaiting for Neo4j to come online, this may take a while..."
   )
+
   # Now we wait for Neo4j to finish starting up
   con <- get_connexion()
-  while (TRUE) {
-    Sys.sleep(12)
-    tryCatch(
-      {
-        if (con$ping() == 200) {
-          break
-        }
-      },
-      error = function(e) {},
-      condition = function(e) {}
-    )
-  }
-  message("Neo4j successfully started.")
-}
 
-#' start_neo4j but used exclusively in unit testing
-#'
-#' @export
-start_test_neo4j <- function() {
-  message("Checking the status of Docker...")
-  # Check if Docker is running
-  if (system("docker system info", ignore.stdout = TRUE, ignore.stderr = TRUE) != 0) {
-    stop("Docker does not appear to be running. Please start Docker and try again.")
-  }
-  message("Docker seems to be running.\n")
-
-  message("Attempting to start Neocache container...")
-  # Start the Docker container
-  # First try to run the existing Docker container
-  if (system("docker start neocache_test_container", ignore.stdout = TRUE, ignore.stderr = TRUE) != 0) {
-    # Container does not exist, create the container
-    if (system("docker run --name neocache_test_container -p7474:7474 -p7687:7687 -d -e NEO4J_AUTH=neo4j/pass -e NEO4J_apoc_export_file_enabled=true -e NEO4J_apoc_import_file_enabled=true -e NEO4J_apoc_import_file_use__neo4j__config=true -e NEO4JLABS_PLUGINS=[\\\"apoc\\\"] neo4j:3.5.21",
-      ignore.stdout = TRUE
-    ) != 0) {
-      stop("Failed to initiate docker container.")
-    }
+  while (try(con$ping()) != 200) {
+    Sys.sleep(3)
   }
 
-  message(
-    "Neocache container successfully launched.\n",
-    "\nWaiting for Neo4j to come online, this may take a while..."
-  )
-  # Now we wait for Neo4j to finish starting up
-  con <- get_connexion()
-  while (TRUE) {
-    Sys.sleep(12)
-    tryCatch(
-      {
-        if (con$ping() == 200) {
-          break
-        }
-      },
-      error = function(e) {},
-      condition = function(e) {}
-    )
-  }
-  message("Neo4j successfully started.")
+  log_debug("Neo4j successfully started.")
 }
 
 
 #' Stops the Neo4j Docker container.
 stop_neo4j <- function() {
-  if (system("docker stop neocache_docker", ignore.stdout = TRUE) != 0) {
+  if (!stop_neocache_docker()) {
     warning("Error returned when attempting to stop docker container.")
   } else {
-    message("Successfully shut down Neo4j and the Neocache Docker container.")
-  }
-}
-
-#' stop_neo4j but used exclusively in unit testing
-stop_neo4j <- function() {
-  if (system("docker stop neocache_test_container", ignore.stdout = TRUE) != 0) {
-    warning("Error returned when attempting to stop docker container.")
-  } else {
-    message("Successfully shut down Neo4j and the Neocache Docker container.")
+    log_debug("Successfully shut down Neo4j and the Neocache Docker container.")
   }
 }
 
@@ -134,15 +72,6 @@ sup4j <- function(query, con = get_connexion()) {
   suppressMessages(call_neo4j(query, con))
 }
 
-tester_a <- function() {
-  assign("val", 3)
-}
-
-tester_b <- function() {
-  print(val)
-}
-
-
 #' This function creates edges en masse between all the nodes provided in the
 #' tbl argument.
 #'
@@ -151,12 +80,12 @@ tester_b <- function() {
 docker_bulk_connect_nodes <- function(tbl) {
   # Create temp file to write the data into
   tmp <- tempfile()
+  on.exit(file.remove(tmp))
 
   # Write the data; we use cat instead of write to eliminate any trailing newline
   cat(paste0("to,from\n", paste0('"', tbl$to, '","', tbl$from, '"', collapse = "\n")), file = tmp)
 
-  # Copy the data into the docker container
-  system(glue("docker cp {tmp} neocache_docker:/var/lib/neo4j/import/data.csv"))
+  copy_csv_to_docker(tmp)
 
   # Add a node for each of the root user's friends and connect the root user to them
   ## This query runs on joe50k in ~2.75 minutes
@@ -164,11 +93,8 @@ docker_bulk_connect_nodes <- function(tbl) {
     "LOAD CSV WITH HEADERS FROM 'file:///data.csv' AS row ",
     "MATCH (to:User {{user_id:row.to}}) MATCH (from:User {{user_id:row.from}}) CREATE (from)-[:FOLLOWS]->(to)"
   )
+
   sup4j(connect_qry)
-
-  file.remove(tmp)
-
-  tbl
 }
 
 
@@ -177,14 +103,15 @@ docker_bulk_connect_nodes <- function(tbl) {
 #' @param user_ids a vector of user_ids to generate MERGE queries for
 docker_bulk_merge_users <- function(user_ids) {
   tmp <- tempfile()
+  on.exit(file.remove(tmp))
 
   cat(paste0("user_id\n", paste0('"', user_ids, '"', collapse = "\n")), file = tmp)
-  system(glue("docker cp {tmp} neocache_docker:/var/lib/neo4j/import/data.csv"))
+
+  copy_csv_to_docker(tmp)
 
   add_qry <- glue("LOAD CSV WITH HEADERS FROM 'file:///data.csv' AS row MERGE (n:User {{user_id:row.user_id}})")
   sup4j(add_qry)
 
-  file.remove(tmp)
 }
 
 
@@ -209,9 +136,17 @@ db_get_friends <- function(user_ids) {
   )
 
   tmp <- tempfile()
-  system(glue("docker cp neocache_docker:/var/lib/neo4j/import/get_friends.csv {tmp}"))
-  results <- readr::read_csv(tmp, col_types = readr::cols(from.user_id = readr::col_character(), to.user_id = readr::col_character()))
-  file.remove(tmp)
+  on.exit(file.remove(tmp))
+
+  pull_friend_data_from_docker(tmp)
+
+  results <- readr::read_csv(
+    tmp,
+    col_types = readr::cols(
+      from.user_id = readr::col_character(),
+      to.user_id = readr::col_character()
+    )
+  )
 
 
   if (length(results) != 2) {
@@ -243,9 +178,17 @@ db_get_followers <- function(user_ids) {
   )
 
   tmp <- tempfile()
-  system(glue("docker cp neocache_docker:/var/lib/neo4j/import/get_friends.csv {tmp}"))
-  results <- readr::read_csv(tmp, col_types = readr::cols(from.user_id = readr::col_character(), to.user_id = readr::col_character()))
-  file.remove(tmp)
+  on.exit(file.remove(tmp))
+
+  pull_friend_data_from_docker(tmp)
+
+  results <- readr::read_csv(
+    tmp,
+    col_types = readr::cols(
+      from.user_id = readr::col_character(),
+      to.user_id = readr::col_character()
+    )
+  )
 
   if (length(results) != 2) {
     return(empty_user_edges())
@@ -254,12 +197,3 @@ db_get_followers <- function(user_ids) {
   tibble(from = results$from.user_id, to = results$to.user_id)
 }
 
-
-#' HELPER FUNCTION USED TO CLEAR THE DATABASE FOR TESTING,
-#' REMOVE THIS FUNCTION ONCE TESTING IS DONE
-clear____db <- function() {
-  con <- get_connexion()
-
-  "MATCH (n) DETACH DELETE n" %>%
-    sup4j(con)
-}
---
+++ R/docker.R
@@ -0,0 +1,36 @@
+# return TRUE for success, FALSE for failure
+
+success <- function(sys_code) {
+  sys_code == 0
+}
+
+find_docker <- function() {
+  code <- system("docker system info", ignore.stdout = TRUE, ignore.stderr = TRUE)
+  success(code)
+}
+
+start_neocache_docker <- function() {
+  code <- system("docker start neocache_docker", ignore.stdout = TRUE, ignore.stderr = TRUE)
+  success(code)
+}
+
+create_neocache_docker <- function() {
+  code <- system(
+    "docker run --name neocache_docker -p7474:7474 -p7687:7687 -d -e NEO4J_AUTH=neo4j/pass -e NEO4J_apoc_export_file_enabled=true -e NEO4J_apoc_import_file_enabled=true -e NEO4J_apoc_import_file_use__neo4j__config=true -e NEO4JLABS_PLUGINS=[\\\"apoc\\\"] neo4j:3.5.21",
+    ignore.stdout = TRUE
+  )
+  success(code)
+}
+
+stop_neocache_docker <- function() {
+  code <- system("docker stop neocache_docker", ignore.stdout = TRUE)
+  succes(code)
+}
+
+copy_csv_to_docker <- function(path) {
+  system(glue("docker cp {path} neocache_docker:/var/lib/neo4j/import/data.csv"))
+}
+
+pull_friend_data_from_docker <- function(path) {
+  system(glue("docker cp neocache_docker:/var/lib/neo4j/import/get_friends.csv {tmp}"))
+}
